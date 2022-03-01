afdbc243e20983d0c47d73b7003a2fff303e50bc
Ada <ada.nowak@icloud.com>
2022-03-01 23:39:57 +0100

    searching improved a bit

--- app.js
+++ app.js
@@ -37,15 +37,15 @@ list.addEventListener("click", (e) => {
 
 const searchTodos = (term) => {
   Array.from(list.children)
-    .filter((todo) => !todo.textContent.includes(term))
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
     .forEach((todo) => todo.classList.add("filtered"));
 
   Array.from(list.children)
-    .filter((todo) => todo.textContent.includes(term))
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
     .forEach((todo) => todo.classList.remove("filtered"));
 };
 
 search.addEventListener("keyup", () => {
-  const term = search.value.trim();
+  const term = search.value.trim().toLowerCase();
   searchTodos(term);
 });
